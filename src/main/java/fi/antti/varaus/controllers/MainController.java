57bb8c95efafc652f963f45f4f2c1d273bd0ab61
Antti Vuorinen <anmivu@utu.fi>
2019-04-18 10:28:36 +0300

    controllit sivuilla liikkumiseen

---
+++ src/main/java/fi/antti/varaus/controllers/MainController.java
@@ -0,0 +1,20 @@
+package fi.antti.varaus.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping({"/", "/welcome"})
+    public String welcome(Model model) {
+        return "welcome";
+    }
+}
---
+++ src/main/java/fi/antti/varaus/controllers/UserRegistrationController.java
@@ -0,0 +1,49 @@
+package fi.antti.varaus.controllers;
+
+import fi.antti.varaus.model.User;
+import fi.antti.varaus.service.SecurityService;
+import fi.antti.varaus.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserRegistrationController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@Valid @ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("error");
+            return "registration";
+        }
+
+        userService.save(userForm);
+
+        securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
+        System.out.println("taalla");
+        return "redirect:/welcome";
+    }
+
+}
