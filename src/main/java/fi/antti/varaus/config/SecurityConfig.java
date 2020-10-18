c66dd2d45fe2fe8e3f2a6de50d7fb2653d13984d
randomapsi <anmivu@utu.fi>
2020-10-19 01:27:49 +0300

    fixing project

--- src/main/java/fi/antti/varaus/config/SecurityConfig.java
+++ src/main/java/fi/antti/varaus/config/SecurityConfig.java
@@ -54,5 +54,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+
     }
+
+
+
+
 }
+
